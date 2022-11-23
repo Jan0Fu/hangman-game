ce669248c1005f2144fcca8350ae14fe1f6e5b9b
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-23 15:46:05 +0100

    Estabilished main elements of program

--- hangman.py
+++ hangman.py
@@ -10,6 +10,15 @@ def hangman():
     used_letters = set()
     user_letter = input("Guess a letter: ").upper()
 
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print("You have already used that character, try something else")
+    else:
+        print("Invalid character, try again")
 
 
 
