4ffeca9a5d5ad3e651b55fd5fd49ac228a1bd76a
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-23 15:59:21 +0100

    Fixed bug

--- hangman.py
+++ hangman.py
@@ -14,16 +14,19 @@ def hangman():
         print("You have used these letters: ", ' '.join(used_letters))
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print("Current word: ", " ".join(word_list))
-        
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
 
-    elif user_letter in used_letters:
-        print("You have already used that character, try something else")
-    else:
-        print("Invalid character, try again")
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print("You have already used that character, try something else")
+        else:
+            print("Invalid character, try again")
+
+hangman()
 
 
 
